5d5e1a3a0ac2be8beedf93619058fbc7955e5ef3
Carlos Martel <cmartelro@gmail.com>
2025-10-18 20:41:00 -0500

    Corrige ACL y activa workflow para despliegue

--- main.tf
+++ main.tf
@@ -1,13 +1,17 @@
 provider "aws" {
-  region = "us-east-1" # region
+  region = "us-east-1"
 }
 
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "mi-bucket-desde-terraform-github" # Debe ser único a nivel global
-  acl    = "private"
+  bucket = "my-bucket-prueba-de-infra54638"
 
   tags = {
     Name        = "MiBucket"
     Environment = "Dev"
   }
+}
+
+resource "aws_s3_bucket_acl" "my_bucket_acl" {
+  bucket = aws_s3_bucket.my_bucket.id
+  acl    = "private"
 }
