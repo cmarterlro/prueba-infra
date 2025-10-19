d461ff57b29c0db9c71a2c4a04524e98b55fd9b0
Carlos Martel <cmartelro@gmail.com>
2025-10-18 23:21:26 -0500

    Configura workflow de Terraform

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "bastion_host" {
   ami           = "ami-0341d95f75f311023" # Amazon Linux 2023 (us-east-1)
-  instance_type = "t2.micro"
+  instance_type = "t2.nano"
   subnet_id     = aws_subnet.public_subnet.id
   associate_public_ip_address = true
 
