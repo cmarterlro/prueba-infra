40bad7193c066be75bef8dd92141a411d27b3479
Carlos Martel <cmartelro@gmail.com>
2025-10-19 10:35:17 -0500

    Actualiza infraestructura para redeploy

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "bastion_host" {
-  ami           = "ami-0341d95f75f311023" # Amazon Linux 2023 (us-east-1)
+  ami           = "ami-0341d95f75f311023" # Amazon Linux 2023 (us-east-1) vf
   instance_type = "t2.nano"
   subnet_id     = aws_subnet.public_subnet.id
   associate_public_ip_address = true
